7b24b53237c88b6900d86c3b13b2329b6d4e075e
Valeriy Bagaveev <valera-bagaveev@mail.ru>
2021-05-16 00:26:20 +0300

    feat: added background for first screen

---
+++ src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+
+function App () {
+  return (
+    <div className="weatherForecast">
+      <h1 className="weatherForecast__title">
+          <span className="weatherForecast__title__top">Weather</span>
+          <span className="weatherForecast__title__bottom">forecast</span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
