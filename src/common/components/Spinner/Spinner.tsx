59f8458cf2016eed790334e6d5c46b670a7c352c
Valeriy Bagaveev <valera-bagaveev@mail.ru>
2021-05-22 19:36:05 +0300

    feat: added spinner and event realization for first api request

---
+++ src/common/components/Spinner/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Spinner.scss'
+
+const Spinner = () => {
+  return (
+        <div className="loadingio-spinner-rolling-05wnqdg7d2fo">
+            <div className="ldio-bemjya7t92s">
+                <div></div>
+            </div>
+        </div>
+  )
+}
+
+export default Spinner
--- src/components/CalcFutForecast/CalcFutForecast.tsx
+++ src/components/CalcFutForecast/CalcFutForecast.tsx
@@ -3,6 +3,7 @@ import OpenWeather from '../../services/open-weather'
 import { COORDINATES } from '../../common/store/data'
 import VisualCard from '../../common/components/VisualCard'
 import './CalcFutForecast.scss'
+import Spinner from '../../common/components/Spinner'
 
 import { ReactComponent as ArrowLeft } from './images/chevron-left.svg'
 import { ReactComponent as ArrowRight } from './images/chevron-right.svg'
@@ -21,8 +22,16 @@ interface IForecastAnswer {
 
 const CalcFutForecast:React.FC<ISelectCityProps> = (props: any) => {
   const [forecastAnswer, setForecastAnswer] = useState([])
+  const [sliderWidth, setSliderWidth] = useState(0)
+  const [sliderItemWidth, setSliderItemWidth] = useState(0)
+  const [currentShift, setCurrentShift] = useState(0)
+  // const [visibleItems, setVisibleItems] = useState(3)
 
   const openWeather = new OpenWeather()
+
+  let sliderElem:any
+  let visualCard:any
+  let slideCount = 0
 
   let lat = ''
   let lon = ''
@@ -33,27 +42,87 @@ const CalcFutForecast:React.FC<ISelectCityProps> = (props: any) => {
       lon = values[1]
     }
   }
+  const setProperties = () => {
+    sliderElem = document.querySelector('.calc-forecast-template__items')
+    visualCard = document.querySelector('.visual-card-template')
+
+    if (sliderElem) {
+      slideCount = sliderElem.childElementCount
+      if (visualCard) {
+        const getMarginRight = parseInt(getComputedStyle(visualCard).marginRight)
+        setSliderItemWidth(visualCard.offsetWidth + getMarginRight)
+        setSliderWidth(sliderItemWidth * slideCount)
+      }
+      sliderElem.style.transform = `translate3d(${currentShift}px, 0, 0)`
+    }
+  }
+
+  const moveLeft = () => {
+    if (currentShift < 0) {
+      setCurrentShift(currentShift + sliderItemWidth)
+      if (sliderElem) {
+        sliderElem.style.transform = `translate3d(${currentShift}px, 0, 0)`
+      }
+    }
+  }
+
+  const moveRight = () => {
+    if (currentShift > -(sliderWidth - 3 * sliderItemWidth)) {
+      setCurrentShift(currentShift - sliderItemWidth)
+      if (sliderElem) { sliderElem.style.transform = `translate3d(${currentShift}px, 0, 0)` }
+    }
+  }
 
   useEffect(() => {
     openWeather.getWeather7Days(lat, lon)
       .then((forecast: any) => {
         setForecastAnswer(forecast)
+        setCurrentShift(0)
       })
   }, [lat, lon])
 
+  useEffect(() => {
+    setProperties()
+    const ShiftByButtons = (event:any) => {
+      if (event.key === 'ArrowLeft') {
+        moveLeft()
+      }
+
+      if (event.key === 'ArrowRight') {
+        moveRight()
+      }
+    }
+    document.addEventListener('keydown', ShiftByButtons)
+    return () => {
+      document.removeEventListener('keydown', ShiftByButtons)
+    }
+  })
+
+  const renderItems = (arr:any) => {
+    return arr.map((el: IForecastAnswer, key:number) => {
+      return (
+          <VisualCard key={key} day={el.day} month={el.month} year={el.year} tmp={el.tmp} icon={el.icon}/>
+      )
+    })
+  }
+
+  const items = renderItems(forecastAnswer)
+
+  if (forecastAnswer.length === 0) {
+    return (
+        <Spinner/>
+    )
+  }
+
   return (
         <div className="calc-forecast-template">
-          <ArrowLeft className="calc-forecast-template__arrow calc-forecast-template__arrow_left"/>
+          <ArrowLeft className="calc-forecast-template__arrow calc-forecast-template__arrow_left" onClick={moveLeft} />
           <div className="calc-forecast-template__items__overlay">
             <div className="calc-forecast-template__items">
-            {forecastAnswer.map((el: IForecastAnswer, index:number) => {
-              return (
-                  <VisualCard key={index} day={el.day} month={el.month} year={el.year} tmp={el.tmp} icon={el.icon}/>
-              )
-            })}
+              {items}
             </div>
           </div>
-          <ArrowRight className="calc-forecast-template__arrow calc-forecast-template__arrow_right"/>
+          <ArrowRight className="calc-forecast-template__arrow calc-forecast-template__arrow_right" onClick={moveRight} />
         </div>
   )
 }
