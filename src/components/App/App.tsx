537577a498c1391274a08b076e3faa67e260d7d6
Valeriy Bagaveev <valera-bagaveev@mail.ru>
2021-05-23 20:10:49 +0300

    fix: refactoring styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,39 +1,30 @@
 import React from 'react'
 import './App.scss'
-// import OpenWeather from '../../services/open-weather'
 import FutureForecast from '../FutureForecast'
 import PastForecast from '../PastForecast'
 
 function App () {
-  // const openWeather = new OpenWeather()
-
-  // openWeather.getWeather7Days(55.796127, 49.106405)
-  //   .then((person: any) => {
-  //     console.log(person)
-  //   })
-  //
-  // openWeather.getWeatherPrevious(55.796127, 49.106405, 1621014561000)
-  //   .then((person: any) => {
-  //     console.log(person)
-  //   })
-
   return (
-    <div className="weatherForecast">
-      <h1 className="weatherForecast__title">
-          <span className="weatherForecast__title__top">Weather</span>
-          <span className="weatherForecast__title__bottom">forecast</span>
-      </h1>
-      <div className="weatherForecast__content">
-          <div className="weatherForecast__content__block">
-              <h2 className="weatherForecast__content__block__subtitle">7 Days Forecast</h2>
-              <FutureForecast/>
-          </div>
-          <div className="weatherForecast__content__block">
-              <h2 className="weatherForecast__content__block__subtitle">Forecast for a Date in the Past</h2>
-              <PastForecast/>
-          </div>
-      </div>
-    </div>
+        <div className="weatherForecast">
+            <h1 className="weatherForecast__title">
+                <span className="weatherForecast__title__top">Weather</span>
+                <span className="weatherForecast__title__bottom">forecast</span>
+            </h1>
+            <div className="weatherForecast__content">
+                <div className="weatherForecast__content__block">
+                    <h2 className="weatherForecast__content__block__subtitle">7 Days Forecast</h2>
+                    <FutureForecast/>
+                </div>
+                <div className="weatherForecast__content__block">
+                    <h2 className="weatherForecast__content__block__subtitle">Forecast for a Date in the Past</h2>
+                    <PastForecast/>
+                </div>
+            </div>
+            <div className="weatherForecast__bottom">
+                <a href="https://mercdev.ru/" className="weatherForecast__bottom__link" target="_blank" rel="noreferrer">C ЛЮБОВЬЮ ОТ MERCURY DEVELOPMENT</a>
+            </div>
+
+        </div>
   )
 }
 
